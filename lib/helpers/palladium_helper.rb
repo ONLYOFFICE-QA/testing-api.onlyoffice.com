ba485bade2460ed9e02393ed7e54393399829a6c
Pavel Lobashov <shockwavenn@gmail.com>
2018-10-26 15:52:57 +0300

    Extract tcm helpers to modules
    Keep it consistance through project

--- lib/helpers/palladium_helper.rb
+++ lib/helpers/palladium_helper.rb
@@ -1,19 +1,21 @@
-# module for initialise palladium object
-module PalladiumHelper
-  def init_palladium(tcm_helper)
-    Palladium.new(host: 'palladium.teamlab.info',
-                  token: palladium_token,
-                  product: tcm_helper.product_name,
-                  plan: tcm_helper.plan_name,
-                  run: tcm_helper.suite_name)
-  rescue StandardError => e
-    OnlyofficeLoggerHelper.log("Cant init palladium. Error: #{e}", 41)
-  end
+module TestingApiOnlyfficeCom
+  # module for initialise palladium object
+  module PalladiumHelper
+    def init_palladium(tcm_helper)
+      Palladium.new(host: 'palladium.teamlab.info',
+                    token: palladium_token,
+                    product: tcm_helper.product_name,
+                    plan: tcm_helper.plan_name,
+                    run: tcm_helper.suite_name)
+    rescue StandardError => e
+      OnlyofficeLoggerHelper.log("Cant init palladium. Error: #{e}", 41)
+    end
 
-  def palladium_token
-    palladium_key = ENV['ONLYOFFICE_PALLADIUM_KEY'] || File.read("#{ENV['HOME']}/.palladium/token")
-    raise 'Palladium key not found' unless palladium_key
+    def palladium_token
+      palladium_key = ENV['ONLYOFFICE_PALLADIUM_KEY'] || File.read("#{ENV['HOME']}/.palladium/token")
+      raise 'Palladium key not found' unless palladium_key
 
-    palladium_key
+      palladium_key
+    end
   end
 end
--- lib/test_manager.rb
+++ lib/test_manager.rb
@@ -1,19 +1,21 @@
 require 'onlyoffice_tcm_helper'
 require_relative 'helpers/palladium_helper'
-# this class help to init palladium and test case manager helper
-class TestManager
-  include PalladiumHelper
+module TestingApiOnlyfficeCom
+  # this class help to init palladium and test case manager helper
+  class TestManager
+    include PalladiumHelper
 
-  def initialize(params = {})
-    params[:suite_name] ||= File.basename(__FILE__)
-    params[:plan_name] ||= 'Unknown Plan Name'
-    params[:product_name] ||= 'api.onlyoffice.com'
-    @tcm_helper = OnlyofficeTcmHelper::TcmHelper.new(params)
-    @palladium = init_palladium(@tcm_helper)
-  end
+    def initialize(params = {})
+      params[:suite_name] ||= File.basename(__FILE__)
+      params[:plan_name] ||= 'Unknown Plan Name'
+      params[:product_name] ||= 'api.onlyoffice.com'
+      @tcm_helper = OnlyofficeTcmHelper::TcmHelper.new(params)
+      @palladium = init_palladium(@tcm_helper)
+    end
 
-  def add_result(example)
-    result = @tcm_helper.parse(example)
-    @palladium.set_result(status: result.status.to_s, description: result.result_message, name: result.case_name)
+    def add_result(example)
+      result = @tcm_helper.parse(example)
+      @palladium.set_result(status: result.status.to_s, description: result.result_message, name: result.case_name)
+    end
   end
 end
