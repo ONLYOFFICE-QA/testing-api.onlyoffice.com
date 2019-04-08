f71ffeda97dc26a1fcd547e2dc734c9907c7f391
Olga Nesterova <40513035+olga-nesterova@users.noreply.github.com>
2019-04-08 16:11:07 +0300

    Variables rename

--- spec/document_builder_example_spec.rb
+++ spec/document_builder_example_spec.rb
@@ -16,29 +16,29 @@ describe 'document_builder_example' do
     end
 
     it 'generate document works' do
-      builder_download = @introduction_page.generate_document_from_script
-      expect(File.file?(builder_download)).to be_truthy
-      parser_builder = OoxmlParser::Parser.parse(builder_download)
-      builder_text = parser_builder.elements[2].character_style_array[0].text
-      expect(builder_text).to match(/^ONLYOFFICE.*Commercial director.*/)
+      file_generated_from_script = @introduction_page.generate_document_from_script
+      expect(File.file?(file_generated_from_script)).to be_truthy
+      parsed_generated_file = OoxmlParser::Parser.parse(file_generated_from_script)
+      text_in_script_example = parsed_generated_file.elements[2].character_style_array[0].text
+      expect(text_in_script_example).to match(/^ONLYOFFICE.*Commercial director.*/)
     end
 
     it 'create document works' do
-      docx_download = @introduction_page.create_docx_from_sample_data
-      expect(File.file?(docx_download)).to be_truthy
-      parser_docx = OoxmlParser::Parser.parse(docx_download)
-      docx_name = parser_docx.elements[1].character_style_array[0].text
-      docx_company_and_position = parser_docx.elements[2].character_style_array[0].text
-      expect(docx_name).to include('John Smith')
-      expect(docx_company_and_position).to match(/^ONLYOFFICE.*Commercial director.*/)
+      sample_docx_file = @introduction_page.create_docx_from_sample_data
+      expect(File.file?(sample_docx_file)).to be_truthy
+      parsed_sample_docx_file = OoxmlParser::Parser.parse(sample_docx_file)
+      default_name_in_docx_file = parsed_sample_docx_file.elements[1].character_style_array[0].text
+      default_company_and_position_in_docx_file = parsed_sample_docx_file.elements[2].character_style_array[0].text
+      expect(default_name_in_docx_file).to include('John Smith')
+      expect(default_company_and_position_in_docx_file).to match(/^ONLYOFFICE.*Commercial director.*/)
     end
 
     it 'create spreadsheet works' do
-      xlsx_download = @introduction_page.create_xlsx_from_sample_data
-      expect(File.file?(xlsx_download)).to be_truthy
-      parser_xlsx = OoxmlParser::Parser.parse(xlsx_download)
-      xlsx_name_company_position = parser_xlsx.worksheets[0].rows[6].cells[0].raw_text
-      expect(xlsx_name_company_position).to match(/.*ONLYOFFICE Commercial director.*John Smith.*/)
+      sample_xlsx_file = @introduction_page.create_xlsx_from_sample_data
+      expect(File.file?(sample_xlsx_file)).to be_truthy
+      parsed_sample_xlsx_file = OoxmlParser::Parser.parse(sample_xlsx_file)
+      default_name_company_position_in_xlsx_file = parsed_sample_xlsx_file.worksheets[0].rows[6].cells[0].raw_text
+      expect(default_name_company_position_in_xlsx_file).to match(/.*ONLYOFFICE Commercial director.*John Smith.*/)
     end
   end
 
