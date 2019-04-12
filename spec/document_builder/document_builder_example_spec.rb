f31710dd3d42c19e8a6588d84a9d175727090d16
Olga Nesterova <40513035+olga-nesterova@users.noreply.github.com>
2019-04-12 17:04:47 +0300

    New tests for document builder generation on integrating page  (#19)
    * Add new test for document builder generation on integrating page
    
    * Changed rubocop rules according to new tests

--- spec/document_builder/document_builder_example_spec.rb
+++ spec/document_builder/document_builder_example_spec.rb
@@ -9,12 +9,12 @@ describe 'document_builder_example' do
     @api_page = @instance.go_to_main_page
   end
 
-  describe 'document_builder_generate' do
+  describe 'document_builder_generate_on_introduction_page' do
     before :each do
       @introduction_page = @api_page.go_to_document_builder_introduction
     end
 
-    it 'generate document works' do
+    it 'generate document on introduction page works' do
       file_generated_from_script = @introduction_page.generate_document_from_script
       expect(File.file?(file_generated_from_script)).to be_truthy
       parsed_generated_file = OoxmlParser::Parser.parse(file_generated_from_script)
@@ -22,7 +22,7 @@ describe 'document_builder_example' do
                  .character_style_array[0].text).to match(/^#{TestData::DEFAULT_COMPANY}.*#{TestData::DEFAULT_POSITION}.*/)
     end
 
-    it 'create document works' do
+    it 'create document on introduction page works' do
       sample_docx_file = @introduction_page.create_docx_from_sample_data
       expect(File.file?(sample_docx_file)).to be_truthy
       parsed_sample_docx_file = OoxmlParser::Parser.parse(sample_docx_file)
@@ -32,7 +32,7 @@ describe 'document_builder_example' do
                  .character_style_array[0].text).to match(/^#{TestData::DEFAULT_COMPANY}.*#{TestData::DEFAULT_POSITION}.*/)
     end
 
-    it 'create spreadsheet works' do
+    it 'create spreadsheet on introduction page works' do
       sample_xlsx_file = @introduction_page.create_xlsx_from_sample_data
       expect(File.file?(sample_xlsx_file)).to be_truthy
       parsed_sample_xlsx_file = OoxmlParser::Parser.parse(sample_xlsx_file)
@@ -41,7 +41,7 @@ describe 'document_builder_example' do
         .to match(/.*#{TestData::DEFAULT_COMPANY} #{TestData::DEFAULT_POSITION}.*#{TestData::DEFAULT_NAME}.*/)
     end
 
-    it 'create document with custom data works' do
+    it 'create document with custom data on introduction page works' do
       @introduction_page.input_name_company_position
       sample_docx_custom_file = @introduction_page.create_docx_from_sample_data
       parsed_sample_custom_docx_file = OoxmlParser::Parser.parse(sample_docx_custom_file)
@@ -51,9 +51,62 @@ describe 'document_builder_example' do
                  .character_style_array[0].text).to match(/^#{TestData::CUSTOM_COMPANY}.*#{TestData::CUSTOM_POSITION}.*/)
     end
 
-    it 'create spreadsheet with custom data works' do
+    it 'create spreadsheet with custom data on introduction page works' do
       @introduction_page.input_name_company_position
       sample_xlsx_custom_file = @introduction_page.create_xlsx_from_sample_data
+      parsed_sample_custom_xlsx_file = OoxmlParser::Parser.parse(sample_xlsx_custom_file)
+      expect(parsed_sample_custom_xlsx_file.worksheets[0]
+                 .rows[6].cells[0].raw_text)
+        .to match(/.*#{TestData::CUSTOM_COMPANY} #{TestData::CUSTOM_POSITION}.*#{TestData::CUSTOM_NAME}.*/)
+    end
+  end
+
+  describe 'document_builder_generate_on_integrating_page' do
+    before :each do
+      introduction_page = @api_page.go_to_document_builder_introduction
+      @integrating_page = introduction_page.open_integrating_document_builder
+    end
+
+    it 'generate document on integrating page works' do
+      file_generated_from_script = @integrating_page.generate_document_from_script
+      expect(File.file?(file_generated_from_script)).to be_truthy
+      parsed_generated_file = OoxmlParser::Parser.parse(file_generated_from_script)
+      expect(parsed_generated_file.elements[2]
+                 .character_style_array[0].text).to match(/^#{TestData::DEFAULT_COMPANY}.*#{TestData::DEFAULT_POSITION}.*/)
+    end
+
+    it 'create document on integrating page works' do
+      sample_docx_file = @integrating_page.create_docx_from_sample_data
+      expect(File.file?(sample_docx_file)).to be_truthy
+      parsed_sample_docx_file = OoxmlParser::Parser.parse(sample_docx_file)
+      expect(parsed_sample_docx_file.elements[1]
+                 .character_style_array[0].text).to include(TestData::DEFAULT_NAME)
+      expect(parsed_sample_docx_file.elements[2]
+                 .character_style_array[0].text).to match(/^#{TestData::DEFAULT_COMPANY}.*#{TestData::DEFAULT_POSITION}.*/)
+    end
+
+    it 'create spreadsheet on integrating page works' do
+      sample_xlsx_file = @integrating_page.create_xlsx_from_sample_data
+      expect(File.file?(sample_xlsx_file)).to be_truthy
+      parsed_sample_xlsx_file = OoxmlParser::Parser.parse(sample_xlsx_file)
+      expect(parsed_sample_xlsx_file.worksheets[0]
+                 .rows[6].cells[0].raw_text)
+        .to match(/.*#{TestData::DEFAULT_COMPANY} #{TestData::DEFAULT_POSITION}.*#{TestData::DEFAULT_NAME}.*/)
+    end
+
+    it 'create document with custom data on integrating page works' do
+      @integrating_page.input_name_company_position
+      sample_docx_custom_file = @integrating_page.create_docx_from_sample_data
+      parsed_sample_custom_docx_file = OoxmlParser::Parser.parse(sample_docx_custom_file)
+      expect(parsed_sample_custom_docx_file.elements[1]
+                 .character_style_array[0].text).to include(TestData::CUSTOM_NAME)
+      expect(parsed_sample_custom_docx_file.elements[2]
+                 .character_style_array[0].text).to match(/^#{TestData::CUSTOM_COMPANY}.*#{TestData::CUSTOM_POSITION}.*/)
+    end
+
+    it 'create spreadsheet with custom data on integrating page works' do
+      @integrating_page.input_name_company_position
+      sample_xlsx_custom_file = @integrating_page.create_xlsx_from_sample_data
       parsed_sample_custom_xlsx_file = OoxmlParser::Parser.parse(sample_xlsx_custom_file)
       expect(parsed_sample_custom_xlsx_file.worksheets[0]
                  .rows[6].cells[0].raw_text)
