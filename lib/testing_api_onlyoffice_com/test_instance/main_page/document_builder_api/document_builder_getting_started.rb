23c7dccedcc3b44f998c3b37a98836ffe32069b0
Olga Nesterova <40513035+olga-nesterova@users.noreply.github.com>
2019-04-12 11:08:10 +0300

    New test for checking download of libraries for Windows and Linux (#16)
    * Added new tests for checking download of libraries for Linux and Windows and added new folder for Document Builder in Spec
    
    * Framework changes according to new tests
    
    * Screenshots changes
    
    * Delete wrong documentation
    
    * Backup gems.locked
    
    * Variables and methods rename
    
    * Xpath changes
    
    * Backup for screenshots in document_builder_integrating.rb

---
+++ lib/testing_api_onlyoffice_com/test_instance/main_page/document_builder_api/document_builder_getting_started.rb
@@ -0,0 +1,48 @@
+require 'httparty'
+require 'onlyoffice_file_helper'
+module TestingApiOnlyfficeCom
+  # https://user-images.githubusercontent.com/40513035/55968227-48e64600-5c84-11e9-8ca6-90d1057918cd.png
+  # /docbuilder/gettingstarted
+  class DocumentBuilderGettingStarted
+    include PageObject
+
+    # actions
+    link(:linux_x64, xpath: '//a[contains(@href, "documentbuilder-x64.tar.gz")]')
+    link(:windows_x64, xpath: '//a[contains(@href, "documentbuilder-x64.zip")]')
+    link(:windows_x86, xpath: '//a[contains(@href, "documentbuilder-x86.zip")]')
+
+    def initialize(instance)
+      @instance = instance
+      super(@instance.webdriver.driver)
+      wait_to_load
+    end
+
+    def wait_to_load
+      @instance.webdriver.wait_until { windows_x86_element.visible? }
+    end
+
+    def download_library_linux_x64_works?
+      linux_x64_element.click
+      path_to_downloaded_library_linux_x64 = @instance.webdriver.download_directory + '/' + TestData::DEFAULT_LIBRARY_LINUX_X64_NAME
+      OnlyofficeFileHelper::FileHelper.wait_file_to_download(path_to_downloaded_library_linux_x64)
+      file_size_linux_x64 = File.size(path_to_downloaded_library_linux_x64)
+      [file_size_linux_x64 > 10_000, file_size_linux_x64]
+    end
+
+    def download_library_windows_x64_works?
+      windows_x64_element.click
+      path_to_downloaded_library_windows_x64 = @instance.webdriver.download_directory + '/' + TestData::DEFAULT_LIBRARY_WINDOWS_X64_NAME
+      OnlyofficeFileHelper::FileHelper.wait_file_to_download(path_to_downloaded_library_windows_x64)
+      file_size_windows_x64 = File.size(path_to_downloaded_library_windows_x64)
+      [file_size_windows_x64 > 10_000, file_size_windows_x64]
+    end
+
+    def download_library_windows_x86_works?
+      windows_x86_element.click
+      path_to_downloaded_library_windows_x86 = @instance.webdriver.download_directory + '/' + TestData::DEFAULT_LIBRARY_WINDOWS_X86_NAME
+      OnlyofficeFileHelper::FileHelper.wait_file_to_download(path_to_downloaded_library_windows_x86)
+      file_size_windows_x86 = File.size(path_to_downloaded_library_windows_x86)
+      [file_size_windows_x86 > 10_000, file_size_windows_x86]
+    end
+  end
+end
---
+++ spec/document_builder/getting_started_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+describe 'document_builder_getting_started' do
+  before :all do
+    @test_manager = TestingApiOnlyfficeCom::TestManager.new(suite_name: 'Document Builder Getting Started', plan_name: @config.to_s)
+  end
+
+  before :each do
+    @instance = TestingApiOnlyfficeCom::TestInstance.new(@config)
+    @api_page = @instance.go_to_main_page
+  end
+
+  describe 'download_libraries' do
+    before :each do
+      introduction_page = @api_page.go_to_document_builder_introduction
+      @getting_started_page = introduction_page.open_getting_started
+    end
+
+    it 'download library for LINUXx64 works' do
+      result, file_size_linux_x64 = @getting_started_page.download_library_linux_x64_works?
+      expect(result).to be_truthy, "Page #{@instance.webdriver.driver.current_url}\n\nFile size: #{file_size_linux_x64}"
+    end
+
+    it 'download library for WINDOWSx64 works' do
+      result, file_size_windows_x64 = @getting_started_page.download_library_windows_x64_works?
+      expect(result).to be_truthy, "Page #{@instance.webdriver.driver.current_url}\n\nFile size: #{file_size_windows_x64}"
+    end
+
+    it 'download library for WINDOWSx86 works' do
+      result, file_size_windows_x86 = @getting_started_page.download_library_windows_x86_works?
+      expect(result).to be_truthy, "Page #{@instance.webdriver.driver.current_url}\n\nFile size: #{file_size_windows_x86}"
+    end
+  end
+
+  after :each do |example|
+    @test_manager.add_result(example)
+    @instance.webdriver.quit
+  end
+end
