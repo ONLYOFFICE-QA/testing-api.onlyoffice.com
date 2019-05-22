7395463c88770f5799daa508db985a1fced3f737
Dmitriy Rotatyy <flamine@list.ru>
2019-05-22 13:01:47 +0300

    Delete unused attr_reader

--- lib/testing_api_onlyoffice_com/test_instance/main_page/document_builder_api/documentation_method_page.rb
+++ lib/testing_api_onlyoffice_com/test_instance/main_page/document_builder_api/documentation_method_page.rb
@@ -4,12 +4,11 @@ module TestingApiOnlyfficeCom
   # https://user-images.githubusercontent.com/40513035/57924882-0e894d80-78af-11e9-9ce3-d3f5eb9a2b23.png
   # /docbuilder/basic
   class DocumentationMethodPage
-    attr_reader :link, :page, :params_exist, :return_exist, :example_exist, :document_exist, :path
+    attr_reader :link, :page, :params_exist, :return_exist, :example_exist, :document_exist
 
     def initialize(editor, current_class, method)
       @link = "#{Config.new.server}/docbuilder/#{ClassNameHelper.cleanup_name(editor)}/#{ClassNameHelper.cleanup_name(current_class)}/#{ClassNameHelper.cleanup_name(method)}"
       @page = Nokogiri::HTML(URI.parse(@link).open)
-      @path = "#{editor}/#{current_class}/#{method}"
       @params_exist ||= !@page.xpath('//*[@class="table"]').empty?
       @return_exist ||= !@page.xpath('//*[@class="param-type"]').empty?
       @example_exist ||= !@page.xpath('//pre').empty?
