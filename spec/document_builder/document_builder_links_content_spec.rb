058fbe0c8502f046e2d6ae008ab0415837f0caa3
Olga Nesterova <40513035+olga-nesterova@users.noreply.github.com>
2019-05-22 10:55:08 +0300

    Added shared_example for checking DocBuilder Documentation content

--- spec/document_builder/document_builder_links_content_spec.rb
+++ spec/document_builder/document_builder_links_content_spec.rb
@@ -8,19 +8,12 @@ describe 'document_builder_documentation_content' do
     @test_manager = TestingApiOnlyfficeCom::TestManager.new(suite_name: 'Document Builder Documentation Links Content', plan_name: @config.to_s)
   end
 
-  TestingApiOnlyfficeCom::BuilderPage.parse_document_entries.each_pair do |editor, class_array|
-    context "#{editor} links content" do
-      class_array.each_pair do |current_class, method_array|
-        it "#{editor}/#{current_class}" do
-          failed_methods = {}
-          method_array.each do |method|
-            page = TestingApiOnlyfficeCom::DocumentationMethodPage.new(editor, current_class, method)
-            next if page.all_elements_exist?
-
-            failed_methods[page.link] = page.not_existed_elements
-          end
-          pending "Not Yet Implemented. Error: #{failed_methods}" unless failed_methods.empty?
-          expect(failed_methods).to be_empty
+  TestingApiOnlyfficeCom::BuilderPage.parse_document_entries.each_pair do |editor, class_hash|
+    class_hash.each_pair do |current_class, method_array|
+      method_array.each do |method|
+        context "#{editor}/#{current_class}/#{method}" do
+          page = TestingApiOnlyfficeCom::DocumentationMethodPage.new(editor, current_class, method)
+          it_behaves_like 'page_content_exist', page
         end
       end
     end
