e612f3b44e7694966541b21e57b1579c3e8d08ae
Olga Nesterova <40513035+olga-nesterova@users.noreply.github.com>
2019-05-22 11:08:47 +0300

    Added shared_example for DocBuilder content (#24)
    * Added status 'Pending' for test 'Document Builder Documentation Links Content'
    
    * Added shared_example for checking DocBuilder Documentation content

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,23 @@ RSpec.configure do |config|
     @config = TestingApiOnlyfficeCom::Config.new
   end
 end
+
+shared_examples_for 'page_content_exist' do |documentation_page|
+  describe 'element_include?' do
+    it "#{documentation_page.path}/Parameters" do
+      expect(documentation_page.params_exist).to be_truthy
+    end
+
+    it "#{documentation_page.path}/Returns" do
+      expect(documentation_page.return_exist).to be_truthy
+    end
+
+    it "#{documentation_page.path}/Example" do
+      expect(documentation_page.example_exist).to be_truthy
+    end
+
+    it "#{documentation_page.path}/Resulting document" do
+      expect(documentation_page.document_exist).to be_truthy
+    end
+  end
+end
