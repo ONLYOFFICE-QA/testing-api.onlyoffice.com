8305c90352070c18e0b68d056c4b31e2717acf9d
Olga Nesterova <40513035+olga-nesterova@users.noreply.github.com>
2019-05-24 13:40:16 +0300

    Added status 'Pending' for DocBuilder documentation (#26)
    * Added status 'Pending'
    
    * Failed tests array is .txt
    
    * Added issue
    
    * added failed_docbuilder_tests method
    
    * Changed list of failed tests
    
    * Added caching

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,19 +17,27 @@ shared_examples_for 'page_content_exist' do |path, documentation_page|
     end
 
     it "#{path}/Parameters" do
+      pending('https://github.com/ONLYOFFICE/api.onlyoffice.com/issues/49') if failed_docbuilder_tests.include?("#{path}/Parameters")
       expect(@documentation_page.params_exist).to be_truthy
     end
 
     it "#{path}/Returns" do
+      pending 'https://github.com/ONLYOFFICE/api.onlyoffice.com/issues/49' if failed_docbuilder_tests.include?("#{path}/Returns")
       expect(@documentation_page.return_exist).to be_truthy
     end
 
     it "#{path}/Example" do
+      pending 'https://github.com/ONLYOFFICE/api.onlyoffice.com/issues/49' if failed_docbuilder_tests.include?("#{path}/Example")
       expect(@documentation_page.example_exist).to be_truthy
     end
 
     it "#{path}/Resulting document" do
+      pending 'https://github.com/ONLYOFFICE/api.onlyoffice.com/issues/49' if failed_docbuilder_tests.include?("#{path}/Resulting document")
       expect(@documentation_page.document_exist).to be_truthy
     end
   end
+end
+
+def failed_docbuilder_tests
+  @failed_docbuilder_tests ||= File.read("#{__dir__}/failed_documentation_content_tests")
 end
