68208211bc77609e452ae92f1df96f8a179490dc
Olga Nesterova <40513035+olga-nesterova@users.noreply.github.com>
2019-05-24 10:40:13 +0300

    Added status 'Pending'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'bundler/setup'
 require 'palladium'
 require 'onlyoffice_tcm_helper'
 require_relative '../lib/testing_api_onlyoffice_com'
+require_relative '../lib/testing_api_onlyoffice_com/failed_documentation_content_tests'
 include TestingApiOnlyfficeCom
 
 RSpec.configure do |config|
@@ -17,18 +18,22 @@ shared_examples_for 'page_content_exist' do |path, documentation_page|
     end
 
     it "#{path}/Parameters" do
+      pending('Not Yet Implemented') if FailedTests::FAILED_TESTS.include?("#{path}/Parameters")
       expect(@documentation_page.params_exist).to be_truthy
     end
 
     it "#{path}/Returns" do
+      pending 'Not Yet Implemented' if FailedTests::FAILED_TESTS.include?("#{path}/Returns")
       expect(@documentation_page.return_exist).to be_truthy
     end
 
     it "#{path}/Example" do
+      pending 'Not Yet Implemented' if FailedTests::FAILED_TESTS.include?("#{path}/Example")
       expect(@documentation_page.example_exist).to be_truthy
     end
 
     it "#{path}/Resulting document" do
+      pending 'Not Yet Implemented' if FailedTests::FAILED_TESTS.include?("#{path}/Resulting document")
       expect(@documentation_page.document_exist).to be_truthy
     end
   end
