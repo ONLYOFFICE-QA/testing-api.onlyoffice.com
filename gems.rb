dd5dec14fb86f789aa2513b55a67711d1102a876
Pavel Lobashov <shockwavenn@gmail.com>
2019-10-01 18:05:53 +0300

    Remove `page-object` as dependency
    This gem is already required by
    `onlyoffice_webdriver_wrapper`
    No need to require it several times

--- gems.rb
+++ gems.rb
@@ -9,7 +9,6 @@ gem 'onlyoffice_file_helper'
 gem 'onlyoffice_tcm_helper', git: 'https://github.com/onlyoffice-testing-robot/onlyoffice_tcm_helper'
 gem 'onlyoffice_webdriver_wrapper', git: 'https://github.com/onlyoffice-testing-robot/onlyoffice_webdriver_wrapper'
 gem 'ooxml_parser', git: 'https://github.com/ONLYOFFICE/ooxml_parser'
-gem 'page-object'
 gem 'palladium', git: 'https://github.com/flaminestone/palladium.git'
 gem 'rspec'
 gem 'rubocop', require: false
