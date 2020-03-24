30b7faf7d931246f66e94f61e87c0251f8cfd00f
Pavel Lobashov <ShockwaveNN@gmail.com>
2020-03-24 15:59:54 +0300

    Use `ooxml_parser` from rubygems, not from source (#149)

--- gems.rb
+++ gems.rb
@@ -8,7 +8,7 @@ gem 'onlyoffice_documentserver_testing_framework', git: 'https://github.com/only
 gem 'onlyoffice_file_helper'
 gem 'onlyoffice_tcm_helper', git: 'https://github.com/onlyoffice-testing-robot/onlyoffice_tcm_helper'
 gem 'onlyoffice_webdriver_wrapper', git: 'https://github.com/onlyoffice-testing-robot/onlyoffice_webdriver_wrapper'
-gem 'ooxml_parser', git: 'https://github.com/ONLYOFFICE/ooxml_parser'
+gem 'ooxml_parser'
 gem 'palladium', git: 'https://github.com/flaminestone/palladium.git'
 gem 'rspec'
 gem 'rubocop', require: false
