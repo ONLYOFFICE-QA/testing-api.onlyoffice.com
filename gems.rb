802f47977a1c75d505b04d6e6afb8a67651671e7
Pavel Lobashov <shockwavenn@gmail.com>
2020-12-04 17:49:14 +0300

    Add requirements of `rspec_passed_time_formatter`
    This gem is required for correct run of tests, since this change:
    https://github.com/ONLYOFFICE/testing-wrata/pull/857

--- gems.rb
+++ gems.rb
@@ -11,6 +11,7 @@ gem 'onlyoffice_webdriver_wrapper'
 gem 'ooxml_parser'
 gem 'palladium', git: 'https://github.com/flaminestone/palladium.git'
 gem 'rspec'
+gem 'rspec_passed_time_formatter'
 gem 'rubocop', require: false
 gem 'rubocop-performance', require: false
 gem 'rubocop-rspec', require: false
