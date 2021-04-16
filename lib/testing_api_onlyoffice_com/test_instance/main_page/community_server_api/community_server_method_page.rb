d56eb122f954095cb699c206466c88b7457355d4
Pavel Lobashov <ShockwaveNN@gmail.com>
2021-04-16 20:32:54 +0300

    Add rake task for generating community server missing docs (#702)

--- lib/testing_api_onlyoffice_com/test_instance/main_page/community_server_api/community_server_method_page.rb
+++ lib/testing_api_onlyoffice_com/test_instance/main_page/community_server_api/community_server_method_page.rb
@@ -7,12 +7,37 @@ module TestingApiOnlyfficeCom
     attr_reader :link, :page, :params_exist, :return_exist, :example_exist, :document_exist
 
     def initialize(current_module, method)
+      @module = current_module
+      @method = method
       @link = "#{Config.new.server}/portals/method/#{ClassNameHelper.cleanup_name(current_module)}/#{ClassNameHelper.cleanup_name(method)}"
       @page = Nokogiri::HTML(URI.parse(@link).open)
       @params_exist ||= !@page.xpath('//div[@id="methodParams"]').empty?
       @example_exist ||= !@page.xpath('//div[@id="methodExample"]/pre').empty?
       @return_exist ||= !@page.xpath('//div[@id="methodReturns"]/p/text()').empty?
       @document_exist ||= !@page.xpath('//div[@id="methodResponse"]/pre').empty?
+      OnlyofficeLoggerHelper.log("Got info about #{full_page_name} page")
+    end
+
+    # @return [String] full page name
+    def full_page_name
+      "#{@module}/#{@method}"
+    end
+
+    # @return [True, False] is page fully documented
+    def fully_documented?
+      missing_info.empty?
+    end
+
+    # @return [String] missing info for method
+    def missing_info
+      return @missing_info if @missing_info
+
+      @missing_info = ''
+      @missing_info += "#{full_page_name}/Parameters\n" unless @params_exist
+      @missing_info += "#{full_page_name}/Example\n" unless @example_exist
+      @missing_info += "#{full_page_name}/Returns\n" unless @return_exist
+      @missing_info += "#{full_page_name}/Resulting document\n" unless @document_exist
+      @missing_info
     end
   end
 end
