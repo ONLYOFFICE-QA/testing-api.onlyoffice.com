01d8e2f01cb02d2b17e847b52e13f7def5767003
Pavel Lobashov <shockwavenn@gmail.com>
2021-04-23 19:57:05 +0300

    Refactoring

--- lib/testing_api_onlyoffice_com/test_instance/main_page/document_builder_api/doc_builder_method_page/doc_builder_method_params.rb
+++ lib/testing_api_onlyoffice_com/test_instance/main_page/document_builder_api/doc_builder_method_page/doc_builder_method_params.rb
@@ -8,10 +8,16 @@ module TestingApiOnlyfficeCom
       method_signature.end_with?('()')
     end
 
+    # @return [Boolean] is params are correctly present on page
     def check_params
-      @page.xpath('//*[@id="methodParams"]').text == "This method doesn't have any parameters." if without_params?
+      return correct_message_no_params? if without_params?
 
       !@page.xpath('//*[@class="table"]').empty?
+    end
+
+    # @return [Boolean] is page contains correct message for no params
+    def correct_message_no_params?
+      @page.xpath('//*[@id="methodParams"]').text == "This method doesn't have any parameters."
     end
   end
 end
