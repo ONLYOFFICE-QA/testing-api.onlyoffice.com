ad2a4982bed92989400e3467d8c425f0f7ee6664
Pavel Lobashov <shockwavenn@gmail.com>
2021-04-26 11:05:23 +0300

    Fix enter text

--- lib/testing_api_onlyoffice_com/test_instance/common_pages/search/search_sidebar.rb
+++ lib/testing_api_onlyoffice_com/test_instance/common_pages/search/search_sidebar.rb
@@ -7,7 +7,7 @@ module TestingApiOnlyfficeCom
     # @param [String] string to find
     # @return [SearchResultPage] page with result
     def search(string)
-      @instance.webdriver.type_text('//div[contains(@class, "search-input")]//input', string)
+      @instance.webdriver.type_text('//div[contains(@class, "search-input")]//input', "#{string}\n")
       SearchResultPage.new(@instance)
     end
   end
