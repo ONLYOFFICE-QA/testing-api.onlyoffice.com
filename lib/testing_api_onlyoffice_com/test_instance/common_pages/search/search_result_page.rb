858f197bbadbf31836d43ce974cb30ab0d6a0818
Pavel Lobashov <shockwavenn@gmail.com>
2021-04-26 11:02:32 +0300

    Add test for search sidebar and result page
    Some test checks for XSS vulnerability

---
+++ lib/testing_api_onlyoffice_com/test_instance/common_pages/search/search_result_page.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+module TestingApiOnlyfficeCom
+  # https://user-images.githubusercontent.com/668524/116047440-c1a67800-a67c-11eb-9ab2-527ac3547f2c.png
+  # https://api.teamlab.info/portals/search?query=mysearch
+  class SearchResultPage
+    def initialize(instance)
+      @instance = instance
+      wait_to_load
+    end
+
+    # Wait for page to load
+    # @return [nil]
+    def wait_to_load
+      @instance.webdriver.wait_until do
+        no_entries_found? || some_entries_found?
+      end
+    end
+
+    # @return [Boolean] is no entries found
+    def no_entries_found?
+      @instance.webdriver.element_visible?(no_result_found_xpath)
+    end
+
+    # @return [Boolean] is some entries found
+    def some_entries_found?
+      @instance.webdriver.element_visible?(search_result_entry_xpath)
+    end
+
+    # @return [Integer] found entries count
+    def search_result_count
+      return 0 if no_entries_found?
+
+      @instance.webdriver.get_element_count(search_result_entry_xpath)
+    end
+
+    private
+
+    # @return [String] no result found message xpath
+    def no_result_found_xpath
+      "//span[text() = 'Search results']/../..//tbody//tr"
+    end
+
+    # @return [String] xpath of search result entry
+    def search_result_entry_xpath
+      "//span[text() = 'No results found']"
+    end
+  end
+end
---
+++ lib/testing_api_onlyoffice_com/test_instance/common_pages/search/search_sidebar.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module TestingApiOnlyfficeCom
+  # Module for search left sidebar
+  module SearchSidebar
+    # Perform search by string
+    # @param [String] string to find
+    # @return [SearchResultPage] page with result
+    def search(string)
+      @instance.webdriver.type_text('//div[contains(@class, "search-input")]//input', string)
+      SearchResultPage.new(@instance)
+    end
+  end
+end
