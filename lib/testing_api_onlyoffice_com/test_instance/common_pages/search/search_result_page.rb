fa4fdd5f5e0733c78054f44c94ccce2b6011eb0d
Pavel Lobashov <shockwavenn@gmail.com>
2021-07-30 15:43:00 +0300

    Change xpath for search not found message
    Change here:
    https://github.com/ONLYOFFICE/api.onlyoffice.com/commit/6ef2c0427dbf1ac08dd0fd5078cbd3faadc5af97#diff-48314673fe808e70ebb9f608fa08d773ee6c5ccb7929b61e006b15600f51f0e7R37

--- lib/testing_api_onlyoffice_com/test_instance/common_pages/search/search_result_page.rb
+++ lib/testing_api_onlyoffice_com/test_instance/common_pages/search/search_result_page.rb
@@ -39,7 +39,8 @@ module TestingApiOnlyfficeCom
 
     # @return [String] no result found message xpath
     def no_result_found_xpath
-      "//span[text() = 'No results found']"
+      "//span[text() = 'No results found'] | " \
+        "//span[(text() = 'No results matching your query could be found')]"
     end
 
     # @return [String] xpath of search result entry
