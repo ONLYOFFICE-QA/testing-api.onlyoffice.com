bfb3402ab9cc154420a596eb13b42ddbeea616f9
Sorokin Maksim <60688343+askonev@users.noreply.github.com>
2023-02-03 17:30:43 +0400

    Describe hidden dependency (#1481)
    * gem rake
    
    * gem wrata-api

--- gems.rb
+++ gems.rb
@@ -10,8 +10,10 @@ gem 'onlyoffice_tcm_helper'
 gem 'onlyoffice_webdriver_wrapper'
 gem 'ooxml_parser'
 gem 'palladium'
+gem 'rake'
 gem 'rspec'
 gem 'rspec_passed_time_formatter'
 gem 'rubocop', require: false
 gem 'rubocop-performance', require: false
 gem 'rubocop-rspec', require: false
+gem 'wrata_api'
