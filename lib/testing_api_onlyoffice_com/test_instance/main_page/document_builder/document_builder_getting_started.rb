0c4c107919808fb8cc2458e7e702c9552375cc62
MaxSorokin <askonev007@gmail.com>
2024-05-20 22:59:59 +0300

    feature: smoke footer links
    * method footer_links checker watir element.visible?
    * method survey_of_external_links return http codes

--- lib/testing_api_onlyoffice_com/test_instance/main_page/document_builder/document_builder_getting_started.rb
+++ lib/testing_api_onlyoffice_com/test_instance/main_page/document_builder/document_builder_getting_started.rb
@@ -6,29 +6,36 @@ module TestingApiOnlyOfficeCom
   # https://github.com/ONLYOFFICE-QA/testing-api.onlyoffice.com/assets/60688343/9bb5c42a-2bde-4bb3-97c6-cd70d7fcfdd9
   # /docbuilder/gettingstarted
   class DocumentBuilderGettingStarted < BuilderPage
-    # External link to docbuilder lib sources
-    elements(:links_native_documentbuidler, xpath: '//a[contains(@href, "builder.aspx")]')
+    elements(:layout_table_footer_links, xpath: '//a[contains(@target, "_blank")]')
 
     def initialize(instance)
       @instance = instance
+      @logger = Logger.new($stdout)
+      @logger.level = Logger::INFO
       super
       wait_to_load
     end
 
     def wait_to_load
-      @instance.webdriver.wait_until { links_native_documentbuidler_elements[0].present? }
+      @instance.webdriver.wait_until { layout_table_footer_links_elements[0].present? }
     end
 
-    def external_links_succeeded?
-      result = []
-      links_native_documentbuidler_elements.each do |element|
-        result.push element.visible?
-        @instance.webdriver.webdriver_error('Not visible element for external link') unless result[-1]
+    def footer_links
+      result = {}
+      layout_table_footer_links_elements.each do |element|
+        result.merge!("#{element.href}": element.visible?)
+      end
+      result
+    end
+
+    def survey_of_external_links
+      result = {}
+      layout_table_footer_links_elements.each do |element|
         uri = URI(element.href)
         res = Net::HTTP.get_response(uri)
-        result.push res.code.include? '200'
+        @logger.info("#{uri}: #{res.code}")
+        result.merge!("#{uri}": res.code)
       end
-      result.all?
     end
   end
 end
