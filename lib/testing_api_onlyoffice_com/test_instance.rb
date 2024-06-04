d932eda89492f05ac316c9c9a9bd65998a0ea512
MaxSorokin <askonev007@gmail.com>
2024-06-04 19:32:55 +0300

    ref: min ref test_instance.rb

--- lib/testing_api_onlyoffice_com/test_instance.rb
+++ lib/testing_api_onlyoffice_com/test_instance.rb
@@ -2,6 +2,7 @@
 
 require_relative 'test_instance/common_pages'
 require_relative 'test_instance/main_page'
+
 module TestingApiOnlyOfficeCom
   # Instance of browser to perform actions
   class TestInstance
