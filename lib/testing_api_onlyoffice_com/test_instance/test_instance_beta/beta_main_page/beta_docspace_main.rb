46dfd12f5fe88eedbf9a3f21a17f94b06d695f5c
MaxSorokin <askonev007@gmail.com>
2024-08-12 19:06:40 +0300

    feature: docspace main page route

---
+++ lib/testing_api_onlyoffice_com/test_instance/test_instance_beta/beta_main_page/beta_docspace_main.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require_relative '../beta_main_page'
+
+module TestingApiOnlyOfficeCom
+
+  # TODO: image & description
+  # class
+  class BetaDocSpaceMainPage
+    include PageObject
+
+    attr_accessor :instance
+
+    def initialize(instance)
+      @instance = instance
+      super(@instance.webdriver.driver)
+      wait_to_load
+    end
+
+    link(:h3_javascript_sdk, xpath: "*//h3/a[contains(@href, 'javascript-sdk')]")
+    link(:javascript_sdk, xpath: "*//a[contains(@class, 'global-navigation__submenu-link') and contains(@href, 'docspace') and contains(@href, 'javascript-sdk')]")
+
+    def wait_to_load
+      h3_javascript_sdk_element.present? and javascript_sdk_element.present?
+    end
+
+    def action_move_to(xpath)
+      element = @instance.webdriver.driver.find_element(:xpath, xpath)
+      action = @instance.webdriver.driver.action
+      action.move_to(element).perform
+    end
+  end
+end
---
+++ spec/beta-api.onlyoffice.com/docspace/docspace_main_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'beta_docspace_main_page' do
+
+  before do
+    @instance = TestingApiOnlyOfficeCom::TestInstance.new(config)
+    @beta_docspace = @instance.go_to_main_page
+                              .go_to_beta
+                              .go_to_beta_docspace
+  end
+
+  it 'class has been created' do
+    expect(Object.const_defined?('BetaDocSpaceMainPage')).to be(true)
+    expect(Object.const_get('BetaDocSpaceMainPage')).to be_a(Class)
+  end
+end
