64105003208648736b845b2ff805ea4aa04f6e1f
MaxSorokin <askonev007@gmail.com>
2024-08-13 16:25:53 +0300

    feature: check breadcrumb

--- lib/testing_api_onlyoffice_com/test_instance/test_instance_beta/beta_main_page/beta_plugins_sdk.rb
+++ lib/testing_api_onlyoffice_com/test_instance/test_instance_beta/beta_main_page/beta_plugins_sdk.rb
@@ -16,11 +16,12 @@ module TestingApiOnlyOfficeCom
       wait_to_load
     end
 
+    nav(:breadcrump, xpath: "*//nav[contains(@class, 'breadcrumb')]")
     div(:content, xpath: "*//div[contains(data-search-container-hidable,'')]/div[contains(@class,'content')]")
 
     def wait_to_load
       @instance.webdriver.wait_until do
-        content_element.present?
+        content_element.present? and breadcrump_element.present?
       end
     end
   end
