7a55a85720ee62639bfe11de4c0b75ec696caaff
MaxSorokin <askonev007@gmail.com>
2024-08-13 14:44:53 +0300

    ref: specs

---
+++ spec/beta-api.onlyoffice.com/beta_main_page/beta_docspace_main_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'beta_docspace' do
+  before do
+    @instance = TestingApiOnlyOfficeCom::TestInstance.new(config)
+    @beta_docspace = @instance.go_to_main_page
+                              .go_to_beta
+                              .go_to_beta_docspace
+  end
+
+  it 'class has been created' do
+    expect(Object.const_defined?('BetaDocSpace')).to be(true)
+    expect(Object.const_get('BetaDocSpace')).to be_a(Class)
+  end
+end
