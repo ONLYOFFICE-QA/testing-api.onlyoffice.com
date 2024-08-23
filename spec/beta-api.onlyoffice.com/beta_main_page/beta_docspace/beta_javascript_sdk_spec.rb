f6f3c00986fd032c2238a6217840660c394261f7
MaxSorokin <askonev007@gmail.com>
2024-08-23 17:36:36 +0300

    feature: quit & test manager

--- spec/beta-api.onlyoffice.com/beta_main_page/beta_docspace/beta_javascript_sdk_spec.rb
+++ spec/beta-api.onlyoffice.com/beta_main_page/beta_docspace/beta_javascript_sdk_spec.rb
@@ -3,12 +3,19 @@
 require 'spec_helper'
 
 describe 'beta_javascript-sdk' do
+  test_manager = TestingApiOnlyOfficeCom::TestManager.new(suite_name: '[beta] javascript sdk', plan_name: config.to_s)
+
   before do
     @instance = TestingApiOnlyOfficeCom::TestInstance.new(config)
     @javascript_sdk = @instance.go_to_main_page
                                .go_to_beta
                                .go_to_beta_docspace
                                .go_to_beta_javascript_sdk
+  end
+
+  after do |example|
+    test_manager.add_result(example)
+    @instance.webdriver.quit
   end
 
   it 'class has been created' do
--- spec/beta-api.onlyoffice.com/beta_main_page/beta_docspace/beta_plugins_sdk_spec.rb
+++ spec/beta-api.onlyoffice.com/beta_main_page/beta_docspace/beta_plugins_sdk_spec.rb
@@ -3,12 +3,19 @@
 require 'spec_helper'
 
 describe 'beta_plugins-sdk' do
+  test_manager = TestingApiOnlyOfficeCom::TestManager.new(suite_name: '[beta] plugins sdk', plan_name: config.to_s)
+
   before do
     @instance = TestingApiOnlyOfficeCom::TestInstance.new(config)
     @pliginssdk = @instance.go_to_main_page
                            .go_to_beta
                            .go_to_beta_docspace
                            .go_to_beta_plugins_sdk
+  end
+
+  after do |example|
+    test_manager.add_result(example)
+    @instance.webdriver.quit
   end
 
   it 'class has been created' do
