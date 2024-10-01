1d327620461ac9542eddb8c985a3ad5901ec116c
MaxSorokin <askonev007@gmail.com>
2024-10-01 16:53:42 +0300

    feature: check img to beta_desktop_editors

--- lib/testing_api_onlyoffice_com/test_instance/documentation_helper/document_entry.rb
+++ lib/testing_api_onlyoffice_com/test_instance/documentation_helper/document_entry.rb
@@ -16,6 +16,65 @@ module TestingApiOnlyOfficeCom
       @name = ClassNameHelper.cleanup_name(current_object)
     end
 
+    def click_by_a_via_href(arg = '')
+      xpath = "//a[@href='#{@link}' #{arg}]"
+      element = @instance.webdriver.get_element(xpath)
+      if xpath.include?('constructor')
+        button = element.find_element(:xpath, './../../../../div/button')
+        @instance.webdriver.click(button)
+      elsif parent_element_exists?(element, :xpath, './../../../../div/span') &&
+            parent_element_exists?(element, :xpath, "./../../../../div[contains(@class, 'tree__twig_closed')]")
+        element.find_element(:xpath, './../../../../div/button').click
+      end
+      @instance.webdriver.click(element)
+      self
+    end
+
+    # Method to check if an element exists
+    def parent_element_exists?(element, by, locator)
+      begin
+        element.find_element(by, locator)
+        true
+      rescue Selenium::WebDriver::Error::NoSuchElementError
+        false
+      end
+    end
+
+    def all_img_exists?
+      # Find all image elements
+      images = @instance.webdriver.driver.find_elements(:tag_name, 'img')
+
+      result = []
+      # Iterate through each image and check its status
+      images.each do |image|
+        src = image.attribute('src')
+
+        if src
+          begin
+            # Make an HTTP request to the image URL
+            uri = URI.parse(src)
+            response = Net::HTTP.get_response(uri)
+
+            # Print the status of each image
+            if response.code.to_i == 200
+              OnlyofficeLoggerHelper.log "Image loaded successfully: #{src}"
+              result.push(true)
+            else
+              OnlyofficeLoggerHelper.log "Image failed to load (status code: #{response.code}): #{src}"
+              result.push(false)
+            end
+          rescue CheckImageError => e
+            putsOnlyofficeLoggerHelper.log "Error checking image: #{src}. Error: #{e.message}"
+            result.push(false)
+          end
+        else
+          OnlyofficeLoggerHelper.log 'Image element without a source.'
+          result.push(false)
+        end
+      end
+      result
+    end
+
     def click_expend
       element = @instance.webdriver.get_element(@xpath_expend)
       @instance.webdriver.click(element)
