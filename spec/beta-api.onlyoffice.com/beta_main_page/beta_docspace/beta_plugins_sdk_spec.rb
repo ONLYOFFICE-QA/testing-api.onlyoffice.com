bb716f55f6c2204998d2a8bb7dcbd2a0f9fe02a3
MaxSorokin <askonev007@gmail.com>
2024-10-03 16:20:12 +0300

    feature: Plugins SDK

--- spec/beta-api.onlyoffice.com/beta_main_page/beta_docspace/beta_plugins_sdk_spec.rb
+++ spec/beta-api.onlyoffice.com/beta_main_page/beta_docspace/beta_plugins_sdk_spec.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 
 describe 'beta_plugins-sdk' do
   test_manager = TestingApiOnlyOfficeCom::TestManager.new(suite_name: '[beta] plugins sdk', plan_name: config.to_s)
+  let(:element_chapter_nav_root) { "*//chapter-navigation[@class='tree']" }
 
   before do
     @instance = TestingApiOnlyOfficeCom::TestInstance.new(config)
@@ -21,5 +22,16 @@ describe 'beta_plugins-sdk' do
   it 'BetaPluginsSDK class has been created' do
     expect(Object.const_defined?('BetaPluginsSDK')).to be(true)
     expect(Object.const_get('BetaPluginsSDK')).to be_a(Class)
+  end
+
+  it 'check all img' do
+    hrefs = @pliginssdk.chapter_nav_hrefs(@instance.webdriver.driver.page_source,
+                                          element_chapter_nav_root)
+
+    hrefs.each do |href|
+      expect(DocumentEntry.new(@instance, href, href)
+                          .click_by_a_via_href("and contains(@class, 'tree__leaf')")
+                          .all_img_exists?).to all(be_truthy)
+    end
   end
 end
